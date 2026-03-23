d62c15fd7532f9ed1935879daef6785451d0b26a
quandxnunxi28 <quandxnunxi28@gmail.com>
2026-03-23 15:11:14 +0700

    add project backend

---
+++ ProjectBackend/CuddleBear/CuddleBear/Models/EmailOtp.cs
@@ -0,0 +1,14 @@
+﻿namespace CuddleBear.Models
+{
+    public class EmailOtp
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string OtpCode { get; set; } = null!;
+
+        public DateTime ExpireAt { get; set; }
+    }
+}
+    
---
+++ ProjectBackend/CuddleBear/CuddleBear/Service/QrService.cs
@@ -0,0 +1,17 @@
+﻿using QRCoder;
+
+namespace CuddleBear.Service
+{
+    public class QrService
+    {
+        public byte[] GenerateQr(string text)
+        {
+            QRCodeGenerator generator = new QRCodeGenerator();
+            QRCodeData data = generator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+            PngByteQRCode qrCode = new PngByteQRCode(data);
+
+            return qrCode.GetGraphic(20);
+        }
+    }
+}
